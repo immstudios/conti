6d834940ddc1343a8cf6097a6145e696185540a3
Martastain <martas@imm.cz>
2017-04-22 00:08:44 +0200

    added thread import, pixel_format setting

--- conti/common.py
+++ conti/common.py
@@ -1,19 +1,30 @@
 import os
 from nxtools import *
 
+#TODO: python version agnostic hacks
+import thread
+
+
 DEVNULL = open(os.devnull, 'w')
+
+#
+# Default settings
+#
 
 def get_settings(**kwargs):
     settings = {
         "pipe_path"     : "pipe.nut",   # Use unique path if you run multichannel setup on one machine
+
+        "width"         : 640,
+        "height"        : 360,
+        "frame_rate"    : 25,
+        "pixel_format"  : "yuv420p",
+
         "video_bitrate" : "800k",       # By default, video quality is quite low...
         "video_preset"  : "fast",       # ... because of my aging development machine.
         "video_profile" : "main",
         "video_codec"   : "libx264",    # By default, software encoder is used.
                                         # You may use h264_nvenc if you have modern nVidia card
-        "width"         : 640,
-        "height"        : 360,
-        "frame_rate"    : 25,
         "gop_size"      : 50,           # Fixed GOP size is usefull for HLS/DASH streaming
         "audio_codec"   : "libfdk_aac", # You probably do not want to change this
         "audio_bitrate" : "128k",
