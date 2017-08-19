eae90cfcca7698069698413fc0c2aad2eea894a2
Martastain <martas@imm.cz>
2017-08-19 16:19:27 +0200

    audio only a and video only output support

--- conti/encoder.py
+++ conti/encoder.py
@@ -47,30 +47,33 @@ class ContiEncoder(object):
         for output_settings in self["outputs"]:
             profile = get_profile(**output_settings)
 
-            if self.afilters:
-                afilters = self.afilters.render()
-                cmd.extend(["-filter:a", afilters])
+            if profile["video_codec"]:
+                if self.vfilters:
+                    vfilters = self.vfilters.render()
+                    cmd.extend(["-filter:v", vfilters])
 
-            if self.vfilters:
-                vfilters = self.vfilters.render()
-                cmd.extend(["-filter:v", vfilters])
+                cmd.extend(["-pix_fmt", profile["pixel_format"]])
+                cmd.extend(["-s", "{}x{}".format(profile["width"], profile["height"])])
+                cmd.extend(["-g", str(profile["gop_size"])])
+                cmd.extend(["-c:v", profile["video_codec"]])
+                cmd.extend(["-b:v", profile["video_bitrate"]])
 
-            cmd.extend(["-pix_fmt", profile["pixel_format"]])
-            cmd.extend(["-s", "{}x{}".format(profile["width"], profile["height"])])
-            cmd.extend(["-g", str(profile["gop_size"])])
-            cmd.extend(["-c:v", profile["video_codec"]])
-            cmd.extend(["-b:v", profile["video_bitrate"]])
-            cmd.extend(["-c:a", profile["audio_codec"]])
-            cmd.extend(["-b:a", profile["audio_bitrate"]])
+                if profile["video_codec"] == "libx264":
+                    cmd.extend(["-preset:v", profile["video_preset"]])
+                    cmd.extend(["-profile:v", profile["video_profile"]])
+                    cmd.extend(["-x264opts", "keyint=50:min-keyint=50:scenecut=-1".format(profile["gop_size"], profile["gop_size"])])
 
-            if profile["video_codec"] == "libx264":
-                cmd.extend(["-preset:v", profile["video_preset"]])
-                cmd.extend(["-profile:v", profile["video_profile"]])
-                cmd.extend(["-x264opts", "keyint=50:min-keyint=50:scenecut=-1".format(profile["gop_size"], profile["gop_size"])])
+                elif profile["video_codec"] == "h264_nvenc":
+                    cmd.extend(["-preset:v", profile["video_preset"]])
+                    cmd.extend(["-strict_gop", "1", "-no-scenecut", "1"])
 
-            elif profile["video_codec"] == "h264_nvenc":
-                cmd.extend(["-preset:v", profile["video_preset"]])
-                cmd.extend(["-strict_gop", "1", "-no-scenecut", "1"])
+
+            if profile["audio_codec"]:
+                if self.afilters:
+                    afilters = self.afilters.render()
+                    cmd.extend(["-filter:a", afilters])
+                cmd.extend(["-c:a", profile["audio_codec"]])
+                cmd.extend(["-b:a", profile["audio_bitrate"]])
 
             default_profile = get_profile()
             for key in profile:
